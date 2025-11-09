c1706f61ba2ceb00db2c001bec985725b4720264
CodeCaven <simon.j.caven@gmail.com>
2025-11-09 17:16:12 +1100

    Init commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+
+// scene
+const scene = new THREE.Scene();
+
+// the camera (and projection)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// the renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+// add-ons (not sure about yet)
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+// a cube (a mesh of geometry and material)
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+// by default the object is at (0,0,0) same as camera, move the camera back a bit
+camera.position.z = 5;
+
+// the animation loop, updatee objects here and render the scene
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
